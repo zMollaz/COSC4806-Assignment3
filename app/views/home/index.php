1c2a84c9f97b125f86a288c9c8a85d88410893ee
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2024-06-10 02:03:16 +0000

    Migrated code for index view from prev assignment

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,8 +3,10 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Hey</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
+                <?php echo date("l jS \of F Y") .
+                '<h1>Welcome ' . $_SESSION["username"] . ' to COSC4806 Assignment#2</h1>'
+                ?>
+                <br>
             </div>
         </div>
     </div>
